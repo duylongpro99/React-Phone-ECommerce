e0b102484143c0270861b84b8c016487213f5ea4
longdaobk17 <ripad3011@gmail.com>
2020-03-17 16:22:19 +0700

    Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 export default class Product extends Component {
+    constructor(props){
+        super(props);
+    }
     render() {
+        const {id, title, price, img, inCart}= this.props.product;
         return (
-            <div>
-                Product
+            <div >
+                <img src = {img} />
             </div>
         )
     }
