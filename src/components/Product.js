1a9225a11fe9a04521e8266b38b33f4e284bc4f9
longdaobk17 <ripad3011@gmail.com>
2020-03-17 19:17:14 +0700

    Gradually

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,9 +9,19 @@ export default class Product extends Component {
     render() {
         const {id, title, price, img, inCart}= this.props.product;
         return (
-            <div >
-                <img src = {img} />
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={console.log('You have clicked on card')}>
+                        <Link to="/details">
+                            <img src={img} alt="Phone" className="card-img-top"/>
+                        </Link>
+                    </div>
+                </div>
+            </ProductWrapper>
         )
     }
 }
+
+const ProductWrapper = styled.div`
+
+` 
