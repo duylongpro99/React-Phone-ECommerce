c617b1e63852314f18722aa1113c4d1384d0c61d
longdaobk17 <ripad3011@gmail.com>
2020-03-21 22:04:17 +0700

    done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,7 +19,7 @@ export default class Cart extends Component {
                                     <Title  name ="Your's" title ="Cart" />
                                     <CartColumns />
                                     <CartList value={value}/>
-                                    <CartTotals value={value} />
+                                    <CartTotals value={value} history = {this.props.history} />
                                 </>
                             );
                         }
